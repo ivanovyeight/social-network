cb3ae909496240d3fefd89b6679c4855213d7001
Akira <51890190+brokfinger@users.noreply.github.com>
2021-02-26 18:28:05 +0200

    Added ajax-hendler for users follow/unfollow

--- account/views.py
+++ account/views.py
@@ -1,9 +1,14 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
-from . forms import LoginForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+
+from account.models import Contact
+from . forms import LoginForm, UserRegistrationForm
+
+from common.decorators import ajax_required
 
 
 @login_required
@@ -24,6 +29,30 @@ def user_detail(request, username):
     user = get_object_or_404(User, username=username, is_active=True)
     return render(request, 'account/user/detail.html',
                   {'section': 'people', 'user': user})
+
+
+@ajax_required
+@require_POST
+@login_required
+def user_follow(request):
+    """
+    Ajax-обработчик добавляет и удаляет пользователей из подписок.
+    """
+    user_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if user_id in action:
+        try:
+            user = User.objects.get(id=user_id)
+            if action == 'follow':
+                Contact.objects.get_or_create(user_from=request.user,
+                                              user_to=user)
+            else:
+                Contact.objects.filter(user_from=request.user,
+                                       user_to=user).delete()
+            return JsonResponse({'status': 'ok'})
+        except User.DoesNotExist:
+            return JsonResponse({'status': 'ok'})
+    return JsonResponse({'status': 'ok'})
 
 
 def register(request):
