261f1c60d4790cdbe9ac0477c967843fa402f772
Akira <51890190+brokfinger@users.noreply.github.com>
2021-02-26 15:52:13 +0200

    Added model 'Contact' and configured gitignore file

--- account/models.py
+++ account/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Contact(models.Model):
+    """
+    Модель для связи пользователей между собой. Так как модель User
+    не модифицирована.
+    """
+
+    user_from = models.ForeignKey(User, related_name='rel_from_set',
+                                  on_delete=models.CASCADE)
+    user_to = models.ForeignKey(User, related_name='rel_to_set',
+                                on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    class Meta:
+        ordering = ('-created', )
+
+    def __str__(self):
+        return f'{self.user_from} подписан на {self.user_to}'
