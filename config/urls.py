cd2c239db40eafff5893b3425d1631cd5f546570
Yaroslav Ivanov <58941602+ivanovyeight@users.noreply.github.com>
2021-03-01 16:05:20 +0200

    Add user profile

--- config/urls.py
+++ config/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('account.urls')),
     path('images/', include(('images.urls', 'images'), namespace='images')),
+    path('account/', include('account.urls')),
 
-]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
