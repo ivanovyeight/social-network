510dca553876f5d0d911f5d6390036014563f274
danilovpy <danylov.sergey1@gmail.com>
2021-03-06 17:41:26 +0200

    changing docker settings

--- Dockerfile
+++ Dockerfile
@@ -4,3 +4,4 @@ WORKDIR /src
 COPY requirements.txt /src/
 RUN pip install -r requirements.txt
 COPY . /src/
+ENTRYPOINT ["sh", "/src/entrypoint.sh"]
--- config/settings.py
+++ config/settings.py
@@ -82,25 +82,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'socialnetwork',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
+ }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
