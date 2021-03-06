8baca4bbcf9ad1ed56f2dd4bef18bbdda9ca2698
danilovpy <danylov.sergey1@gmail.com>
2021-03-06 18:12:52 +0200

    adding back postgres setting(removing localhost)

--- config/settings.py
+++ config/settings.py
@@ -82,11 +82,22 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': BASE_DIR / 'db.sqlite3',
-     }
- }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
+    }
+}
+
+#DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
