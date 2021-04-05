84c3aaddad3f9fb8118baaa312ba8a5f254f75ea
Yaroslav Ivanov <58941602+ivanovyeight@users.noreply.github.com>
2021-04-05 22:48:02 +0300

    Update urls

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,9 @@
-from django.urls import path
 from django.contrib.auth import views as auth_views
-from . import views
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from django.urls import path
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
+
+from account import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
@@ -18,7 +17,6 @@ urlpatterns = [
 
     path('timeline/', views.timeline, name='timeline'),
 
-
     path('users/', views.user_list, name='user_list'),
     path('users/<username>/', views.user_detail, name='user_detail'),
 
@@ -28,11 +26,4 @@ urlpatterns = [
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-
-    # DEPRECATED
-    # path('', views.dashboard, name='dashboard'),
-    # path('login/', auth_views.LoginView.as_view(), name='login'),
-    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    # path('edit/', views.edit, name='edit'),
-    
 ]
